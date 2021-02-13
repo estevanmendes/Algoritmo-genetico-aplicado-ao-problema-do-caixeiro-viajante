0e6a92c39f3927797433ccdd00129110197ea6e1
estevanmendes <esteva.amazonas@gmail.com>
2021-02-13 12:15:28 -0300

    metodo fitness update

--- ga.py
+++ ga.py
@@ -9,21 +9,39 @@ class Genetic_algoruthm():
         self.crossover_prob=crossover_prob
         self.cost_matrix=cost_matrix
         self.seed=random_seed
-
+        
         np.random.seed(random_seed)
 
         if not cost_matrix:
             self.cost_matrix=np.random.rand(number_of_city,number_of_city)
 
-            
-    def fitness(self,cost_matrix=False):
+
+        initial_pop=np.tile(np.arange(1,number_of_city+1),(population_size,1))
+
+        for i in range(population_size):
+            np.random.shuffle(initial_pop[i,1:-1])
+
+        self.popolation=initial_pop
+        self.fitness=np.zeros(population_size)
+
+
+    def fitness(self,cost_matrix=False,maxmization=False):
+        fitness_matrix=1./self.cost_matrix
+        self.fitness_matrix=fitness_matrix
+
+        for i in range(self.popolation):
+            for j in range(self.number_of_city):
+                self.fitness[i]+=fitness_matrix[self.popolation[j:j+1]]
+
         pass
 
-    
-        
-class Crossover():
-    def __init___(self,):
+    def run(self,generations=10):
         pass
+
+    def Mutation(self):
+        pass
+
+class Crossover(Genetic_algoruthm):
 
     def SCRX(self):
         pass
